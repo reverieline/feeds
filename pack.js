6f011f10d4c372c849a78fa164134ecd9aa8b79e
Alex Gavr <reverieline@gmail.com>
2021-08-11 21:50:55 +0300

    upd

--- pack.js
+++ pack.js
@@ -2,5 +2,5 @@ const fs = require("fs");
 
 const fetcher = require("./fetchers/elementy.js");
 
-const json = JSON.stringify(fetcher, (k,v) => typeof v === "function" ? "" + v : v);
+const json = JSON.stringify(fetcher, (k,v) => typeof v === "function" ? "(" + v + ")()" : v);
 fs.writeFileSync("./feeds.json",json);
